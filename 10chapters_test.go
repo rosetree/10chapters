17d2d3531f868895152e51a5151a1ecb8e527214
Micha Rosenbaum <micha@rosetree.de>
2018-08-07 11:32:50 +0200

    Add test for generateListChapters function

--- 10chapters_test.go
+++ 10chapters_test.go
@@ -28,3 +28,38 @@ func TestDaysSince(t *testing.T) {
 		t.Error("daysSince with unknown date format should be -1, was", sinceGer)
 	}
 }
+
+// TestGenerateListChapters checks the lengths of the gernated chapter
+// lists and tests for a few expected values.
+func TestGenerateListChapters(t *testing.T) {
+	lists := [10][]book {
+		[]book{ book{"One", 1}, book{"Two", 2} },
+		[]book{ book{"Two", 2}, book{"One", 1} },
+		[]book{ book{"None", 0} },
+		[]book{ book{"abc", 26} },
+		[]book{ book{"abc", 26} },
+		[]book{ book{"abc", 26} },
+		[]book{ book{"abc", 26} },
+		[]book{ book{"abc", 26} },
+		[]book{ book{"abc", 26} },
+		[]book{ book{"abc", 26} },
+	}
+
+	chapters := generateListChapters(lists)
+
+	lengths := []int{ 3, 3, 0, 26, 26, 26, 26, 26, 26, 26 }
+	for i := 0; i < 10; i++ {
+		if len(chapters[i]) == lengths[i] {
+			continue
+		}
+		t.Errorf("Length of list %d doesn’t match, expected %d, was %d",
+			i, lengths[i], len(chapters[i]))
+	}
+
+	if chapters[0][1] != "Two 1" {
+		t.Error("chapters[0][1] should be “Two 1”, was", chapters[0][1])
+	}
+	if chapters[1][1] != "Two 2" {
+		t.Error("chapters[1][1] should be “Two 2”, was", chapters[1][1])
+	}
+}
