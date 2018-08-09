84528303b844c9b652251a99732173bd23e52a5d
Micha Rosenbaum <micha@rosetree.de>
2018-08-09 12:01:40 +0200

    Add handling of date-started, advanced and skipped days to webserver

--- 10chapters.go
+++ 10chapters.go
@@ -65,10 +65,40 @@ func serve(chapters [10][]string) {
 			return
 		}
 
-		day, err := strconv.ParseInt(r.FormValue("day"), 10, 64)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+		var day int64
+
+		dayParam := r.FormValue("day")
+		dateParam := r.FormValue("started")
+
+		if dayParam != "" {
+			day, err = strconv.ParseInt(dayParam, 10, 64)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+
+		if dayParam == "" && dateParam != "" {
+			var daysAdvanced, daysSkipped int64
+
+			advancedParam := r.FormValue("advanced")
+			skippedParam := r.FormValue("skipped")
+
+			currentDay, err := daysSince(dateParam)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			// TODO: Handle these errors
+			daysAdvanced, _ = strconv.ParseInt(advancedParam, 10, 64)
+			daysSkipped, _ = strconv.ParseInt(skippedParam, 10, 64)
+
+			day = int64(currentDay) + daysAdvanced - daysSkipped
+			if day < 1 {
+				http.Error(w, "Error: Cannot create lists for a negative day\n", http.StatusBadRequest)
+				return
+			}
 		}
 
 		w.Header().Set("Content-Type", "text/plain")
